1082cefc90f113ace3959b1eef9910990f8497d5
Yudi Ren <yudi.ren@synsense.ai>
2023-11-07 11:36:19 +0800

    add: imcomplete code

---
+++ common/loadShader.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_LOAD_SHADER_H
+#define COMMON_LOAD_SHADER_H
+
+// Include GLEW. Always include it before gl.h and glfw3.h, since it's a bit magic.
+#include <GL/glew.h>
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+
+#endif
